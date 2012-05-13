c08b74808db4978f234b5d9018e7cceb97d31232
Reshad Noorzay <rnoorzay@gmail.com>
2012-05-13 05:58:27 -0400

    updating image link

--- client/script.js
+++ client/script.js
@@ -1,20 +1,13 @@
 // Client-side JavaScript, bundled and sent to client.
 
 // Define Minimongo collections to match server/publish.js.
-Lists = new Meteor.Collection("lists");
 Orders = new Meteor.Collection("orders");
-
-// ID of currently selected list
-Session.set('list_id', null);
 
 // Name of currently selected tag for filtering
 Session.set('tag_filter', null);
 
 // When adding tag to a todo, ID of the todo
 Session.set('editing_addtag', null);
-
-// When editing a list name, ID of the list
-Session.set('editing_listname', null);
 
 // When editing todo text, ID of the todo
 Session.set('editing_itemname', null);
