aaec0cbc7f2edbbc7bc936d7f28c90f5ef777389
Reshad Noorzay <rnoorzay@gmail.com>
2012-05-13 12:12:01 -0400

    updated function to add orders

--- order.js
+++ order.js
@@ -7,6 +7,30 @@ if (Meteor.is_client) {
     Template.Orders.orders = function() {
         return Orders.find();
     };
+
+   var addOrder = function(){ 
+        var timestamp = new Date();
+        var now = timestamp.toString('yyyy-MM-dd');
+        Orders.insert({
+                year: 2010,
+                model: "Honda",
+                partInfo: "right side door",
+                partPrice: 200,
+                customerName: "Reshad Noorzay",
+                customerPhone: "(310) 555-5555",
+                status: {
+                        statusType: "Quote",
+                        statusLabel: ""
+                },
+                assignedTo: "Najeem",
+                timestamp: now,
+                comments: { 
+                        comment: "need to pull from yard",
+                        commenter: "Fahim",
+                        commentTime: now
+                        }
+                });
+   };
 }
 // On server startup, create some orders if the database is empty.
 if (Meteor.is_server) {
@@ -18,10 +42,11 @@ Meteor.startup(function () {
     var customers = ["ABC Company", "XYZ Company", "Local Auto Sales", "Bros. Auto Parts", "Big Co Auto Dealers"]; 
     var phones = "555-555-5555"; 
     var statusTypes = ["Quote", "Pending", "Pick up", "Delivery", "Quote"]; 
-    var statusLabels = ["", "warning", "notice", "success", "warning"];
+    var statusLabels = ["", "warning", "notice", "success", ""];
     var assignees = ["Najeem", "Juan", "Isaac", "Fahim", "Shiraz"]; 
     var comment = "This is a good part and we should sell quickly";
-    var timestamp = (new Date()).getTime();
+    var timestamp = new Date();
+    var now = timestamp.toString('yyyy-MM-dd');
     var year = 2006;
     for(var i=0; i < 5; i++){
     Orders.insert({
@@ -35,12 +60,12 @@ Meteor.startup(function () {
                         statusType: statusTypes[i],
                         statusLabel: statusLabels[i]
                         },
-                assigned: assignees[i],
-                timestamp: timestamp,
+                assignedTo: assignees[i],
+                timestamp: now,
                 comments: { 
                         comment: comment,
                         commenter: assignees[0],
-                        commentTime: timestamp
+                        commentTime: now
                         }
                 });
                 timestamp += 1; // ensure unique timestamp.
